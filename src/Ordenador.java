394599c4581d22b468b32d597df6ad37becb6618
Filipe Souza <filipesouza005@gmail.com>
2019-06-12 19:37:40 -0300

    Melhora da eficiência do radixsort

--- src/Ordenador.java
+++ src/Ordenador.java
@@ -155,9 +155,9 @@ class Ordenador {
         }
         for (int i = 0; i < digitos; i++) {
             List<List<Integer>> subarrays = new ArrayList<List<Integer>>();
+            int potencia = (int) Math.pow(10, i);
             for (int j = 0; j < 10; j++) subarrays.add(new ArrayList<Integer>());
-            for (int numero : array) subarrays.get((int) (numero % Math.pow(10, i + 1) / Math.pow(10, i))).add(numero);
-
+            for (int numero : array) subarrays.get(numero % potencia * 10/ potencia).add(numero);
             int j = 0;
             for (List<Integer> subarray : subarrays) {
                 for (int numero : subarray) {
@@ -165,9 +165,10 @@ class Ordenador {
                     j++;
                 }
             }
-
         }
     }
+
+
 
     static void insertionSort(int[] array) {
         for(int i = 1; i < array.length; i++){
